d0ed05694cf0eb71b1730859cff042554081f7eb
Ben Worth <u3243222@uni.canberra.edu.au>
2023-10-02 12:15:39 +1100

    misc updates

--- scripts/talker.py
+++ scripts/talker.py
@@ -7,19 +7,19 @@ from std_msgs.msg import Float64
 
 destination_topic = '/blocky/controllers/position/joint1/command' #the topic to publish to
 rate_hz = 2 #the rate at which to publish
-cmd1_rad = 0 #the first destination in radians
-cmd2_rad = 1 #the second destination in radians
+cmd1_data = 1 #the first command
+cmd2_data = 0 #the second command
 
 def talker():
     pub = rospy.Publisher(destination_topic, Float64, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     ros_rate = rospy.Rate(rate_hz)
     while not rospy.is_shutdown():
-        rospy.loginfo("publishing %f to %s", cmd1_rad, destination_topic)
-        pub.publish(cmd1_rad)
+        rospy.loginfo("publishing %f to %s", cmd1_data, destination_topic)
+        pub.publish(cmd1_data)
         ros_rate.sleep()
-        rospy.loginfo("publishing %f to %s", cmd2_rad, destination_topic)
-        pub.publish(cmd2_rad)
+        rospy.loginfo("publishing %f to %s", cmd2_data, destination_topic)
+        pub.publish(cmd2_data)
         ros_rate.sleep()
 
 if __name__ == '__main__':
